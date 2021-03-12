2c36a764d468ab5d9491af99388f1337b8fd302a
Caitlin Bond <caitlin@kumina.nl>
2021-03-12 17:55:34 +0100

    underscore func updated

--- hangman.py
+++ hangman.py
@@ -13,20 +13,12 @@ class Hangman:
         # getpass will make sure the input wont be visible
         answer = getpass.getpass("why don't you give me a word?:")
         # Makes a list of the word the user inputted
-        list_of_letters = list(answer)
+        self.list_of_letters = list(answer)
         # Counts the objects in the list
-        self.number_of_letters = len(list_of_letters)
+        self.number_of_letters = len(self.list_of_letters)
 
     def under_score(self):
-        underscore = "_"
-        number_of_underscore = 0
-        # prints underscores until thy are equal to the number of letters
-        while self.number_of_letters != number_of_underscore:
-            # Prints a underscore
-            # "end" ensures the underscores are printed next to each other
-            print(underscore, end=" ")
-            # adds 1 to variable
-            number_of_underscore += 1
+        print("_ " * self.number_of_letters)
 
 if __name__ == '__main__':
     a = Hangman()
