0b3742231e9e0610364d834a1e4f512dde717349
Caitlin Bond <caitlin@kumina.nl>
2021-03-13 15:15:59 +0100

    added lives and TODO's

--- hangman.py
+++ hangman.py
@@ -1,15 +1,17 @@
 import getpass
+import sys
 
 
 class Hangman:
 
     def __init__(self):
-        pass
+        self.lives = 12
 
     def play(self):
         self.input_word()
         self.under_score()
         self.compare_input()
+        self.amount_of_lives()
 
     def input_word(self):
         print("Hallo welcome to the hangman game.")
@@ -20,6 +22,7 @@ class Hangman:
         self.list_of_letters = list(answer)
         # Counts the objects in the list
         self.number_of_letters = len(self.list_of_letters)
+        print("The other player will have", self.lives, "attempts to guess this", self.number_of_letters, "-letter word")
 
     def under_score(self):
         print("_ " * self.number_of_letters)
@@ -33,12 +36,29 @@ class Hangman:
             if letter in self.list_of_letters:
                 print("true")
             else:
+                self.lives = self.lives - 1
                 print("fals")
         # Checks if the given words are the same
         elif list_letter == self.list_of_letters:
             print("correct you win")
+            sys.exit(0)
         else:
+            self.lives = self.lives - 1
             print("nee")
+
+    def amount_of_lives(self):
+        while self.lives > 0:
+            print(self.lives)
+            self.compare_input()
+        else:
+            print("you failed")
+            sys.exit(0)
+
+# TODO  show letters you guest correct
+# TODO  place letter on correct position
+# TODO  exit when all letter are guessed correct OR you will just need to enter the whole word
+# TODO  make drawings of the hanging man
+# TODO  show correct drawings of the hangman with the amount of lives left
 
 
 if __name__ == '__main__':
