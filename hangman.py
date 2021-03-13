13198da864802f4fd37d905f00f2838b211832f1
Caitlin Bond <caitlin@kumina.nl>
2021-03-13 13:33:04 +0100

    checks if the given input is in the word

--- hangman.py
+++ hangman.py
@@ -3,9 +3,13 @@ import getpass
 
 class Hangman:
 
+    def __init__(self):
+        pass
+
     def play(self):
         self.input_word()
         self.under_score()
+        self.letter_in_list()
 
     def input_word(self):
         print("Hallo welcome to the hangman game.")
@@ -19,6 +23,17 @@ class Hangman:
 
     def under_score(self):
         print("_ " * self.number_of_letters)
+
+    def letter_in_list(self):
+        letter = input()
+        # Checks if the given letter is in the list of the word
+        if letter in self.list_of_letters:
+            print("true")
+        else:
+            print("fals")
+
+
+
 
 if __name__ == '__main__':
     a = Hangman()
